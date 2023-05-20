e6882422b40d2004ee53296bacb8fab8fc68b082
mariaperez15 <123765659+mariaperez15@users.noreply.github.com>
2023-05-20 12:22:22 +0000

    avances js

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [selected, setSelected] = useState ("red");
+
 	return (
 		<div className="semaforo">
-			<div className="light red"></div>
-			<div className="light yellow"></div>
-			<div className="light green"></div>
+			<div className={"light red" + (selected === "red" ? "glow" : "")}></div>
+			<div className={"light yellow" + (selected === "yellow" ? "glow" : "")}></div>
+			<div className={"light green" + (selected === "green" ? "glow" : "")}></div>
 		
 		</div>
 	);
